b3b673d3f05b413bd5900346bd3df79bd2391de6
James Corey <jmc-git@neniam.net>
2025-12-27 23:58:23 -0800

    qrprogress

--- bizcard/gen_qr_code.sh
+++ bizcard/gen_qr_code.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-qrencode -t SVG -o qrcode.svg "https://crdrlabs.github.io/pubdocs/links.html"
+#qrencode -t SVG -o qrcode.svg "https://crdrlabs.github.io/pubdocs/links.html"
+qrencode -v 2 -t SVG -m 0 -o qrcode.svg "https://crdrlabs.org/links.html"
+#qrencode -v 1 -t SVG -o qrcode.svg "WWW.CRDRLABS.ORG"
